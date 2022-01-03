66b93cadd0e81764ba7a2215687215fa4e6e4bca
momenkittaneh <momen.kittaneh93@outlook.com>
2022-01-04 00:22:10 +0200

    asd

--- Restaurant_Ordering_System/settings.py
+++ Restaurant_Ordering_System/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#6$hd2+#%=ufu(a9za0q)zpb#cyw#pjqc6w(cr&kxjh*-r*$&-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com"]
+
 
 
 # Application definition
