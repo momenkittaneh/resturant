3de90a09f9e4d41da4d0867223410880b88e0a8d
momenkittaneh <momen.kittaneh93@outlook.com>
2022-01-04 11:30:45 +0200

    asd

--- Restaurant_Ordering_System/settings.py
+++ Restaurant_Ordering_System/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#6$hd2+#%=ufu(a9za0q)zpb#cyw#pjqc6w(cr&kxjh*-r*$&-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['3.89.222.153']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, '.static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     )
